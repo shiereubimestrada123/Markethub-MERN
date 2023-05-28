fae7c4ddece0a43f3bfc7cc07253553c735b9361
bim <shiereubim.estrada@gmail.com>
2023-05-28 23:53:19 +0800

    Add register and login route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig');
 const port = process.env.PORT || 5000;
+
+const usersRoute = require('./routes/usersRoute');
+
+app.use('/api/users', usersRoute);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
