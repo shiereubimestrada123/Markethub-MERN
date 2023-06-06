f75858b6efc7f35dc4a0b080ee54d0a8c3fab8e5
bim <shiereubim.estrada@gmail.com>
2023-06-06 15:27:19 +0800

    Add product form

--- client/src/pages/Profile/Products/ProductsForm.jsx
+++ client/src/pages/Profile/Products/ProductsForm.jsx
@@ -1,6 +1,40 @@
-import { Modal } from 'antd'
+import { useRef } from 'react';
+import { Modal, Tabs, Form, Input, Row, Col } from 'antd';
+import TextArea from "antd/es/input/TextArea";
+
+const additionalThings = [
+  {
+    label: "Bill Available",
+    name: "billAvailable",
+  },
+  {
+    label: "Warranty Available",
+    name: "warrantyAvailable",
+  },
+  {
+    label: "Accessories Available",
+    name: "accessoriesAvailable",
+  },
+  {
+    label: "Box Available",
+    name: "boxAvailable",
+  },
+];
+
+const rules = [
+  {
+    required: true,
+    message: "Required",
+  },
+];
 
 const ProductsForm = ({ showProductForm, setShowProductForm }) => {
+  const formRef = useRef(null);
+
+  const onFinish = async (values) => {
+    console.log(values)
+  };
+
   return (
     <>
       <Modal
@@ -8,8 +42,77 @@ const ProductsForm = ({ showProductForm, setShowProductForm }) => {
         open={showProductForm}
         onCancel={() => setShowProductForm(false)}
         centered
+        width={1000}
+        okText="Save"
+        onOk={() => {
+          formRef.current.submit();
+        }}
       >
-        <h1>Products Form</h1>
+        <Tabs>
+          <Tabs.TabPane tab="General" key="1">
+            <Form layout="vertical" ref={formRef} onFinish={onFinish}>
+              <Form.Item label="Name" name="name" rules={rules}>
+                <Input type="text" />
+              </Form.Item>
+              <Form.Item label="Description" name="description" rules={rules}>
+                <TextArea type="text" />
+              </Form.Item>
+
+              <Row gutter={[16, 16]}>
+                <Col span={8}>
+                  <Form.Item label="Price" name="price" rules={rules}>
+                    <Input type="number" />
+                  </Form.Item>
+                </Col>
+
+                <Col span={8}>
+                  <Form.Item label="Category" name="category" rules={rules}>
+                    <select>
+                      <option value="">Select</option>
+                      <option value="electronics">Electronics</option>
+                      <option value="fashion">Fashion</option>
+                      <option value="home">Home</option>
+                      <option value="sports">Sports</option>
+                    </select>
+                  </Form.Item>
+                </Col>
+
+                <Col span={8}>
+                  <Form.Item label="Age" name="age" rules={rules}>
+                    <Input type="number" />
+                  </Form.Item>
+                </Col>
+              </Row>
+
+              <div className="flex gap-10">
+                {additionalThings.map((item) => {
+                  return (
+                    <Form.Item
+                      key={item.name}
+                      label={item.label}
+                      name={item.name}
+                      valuePropName="checked"
+                    >
+                      <Input
+                        type="checkbox"
+                        value={item.name}
+                        onChange={(e) => {
+                          formRef.current.setFieldsValue({
+                            [item.name]: e.target.checked,
+                          });
+                        }}
+                        checked={formRef.current?.getFieldValue(item.name)}
+                      />
+                    </Form.Item>
+                  );
+                })}
+              </div>
+            </Form>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Images" key="2">
+            <h1>images</h1>
+          </Tabs.TabPane>
+        </Tabs>
       </Modal>
     </>
   )
