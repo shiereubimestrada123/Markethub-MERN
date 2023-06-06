34e3d541fd991ac401bfa911c1f3d2e73bdb2303
bim <shiereubim.estrada@gmail.com>
2023-06-06 18:32:34 +0800

    Add products api

--- server/server.js
+++ server/server.js
@@ -6,7 +6,9 @@ const dbConfig = require('./config/dbConfig');
 const port = process.env.PORT || 5000;
 
 const usersRoute = require('./routes/usersRoute');
+const productsRoute = require('./routes/productsRoute');
 
 app.use('/api/users', usersRoute);
+app.use('/api/products', productsRoute);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
